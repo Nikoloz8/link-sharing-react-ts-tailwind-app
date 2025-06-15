1fe68dde4d878625b27cc43a6fb8c64a30c01879
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-15 18:05:51 +0400

    add styles, pages, layouts folder, write starter heading styles and router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Register from './pages/Register'
+import Layout from './layouts/Layout'
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    path: "/",
+    children: [
+      {
+        element: <Register />,
+        path: "/register"
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/styles/tailwind.ts
@@ -0,0 +1,10 @@
+export default function tailwind() {
+
+    const H1 = "text-[3.2rem] font-[700] text-[#333333] leading-[150%]"
+    const H2 = "text-[1.6rem] font-[600] text-[#333333] leading-[150%]"
+    const P1 = "text-[1.6rem] font-[400] text-[#333333] leading-[150%]"
+    const P2 = "text-[1.2rem] font-[400] text-[#333333] leading-[150%]"
+    
+
+    return {H1, H2, P1, P2} 
+}
