eb353b01cf34fe60eb587992bf725b3f6d9e7b09
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-15 19:46:58 +0400

    add error states in register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,7 +3,7 @@ import tailwind from "../styles/tailwind"
 
 export default function Register() {
 
-    const { register, watch } = useForm({
+    const { register, watch, setError, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
             emailAddress: "",
             createPassword: "",
@@ -13,24 +13,41 @@ export default function Register() {
 
     console.log(watch())
 
+    console.log(errors)
+
     const { H1, P1, P2, H2, inputStyle } = tailwind()
+
+    const onSubmit = () => {
+        if (watch().createPassword !== watch().confirmPassword || watch().confirmPassword.trim() === "") {
+            setError("createPassword", { message: "Please check again" })
+        }
+        if (watch().emailAddress.trim() === "") {
+            setError("emailAddress", { message: "Can’t be empty" })
+        }
+    }
 
     return (
         <div className="flex flex-col gap-[50px] items-center">
             <img src="/images/logo-devlinks-large.svg" alt="" />
-            <form action="" className="bg-[#FFFFFF] flex flex-col gap-[40px] p-[40px] rounded-[12px]" onSubmit={(e) => e.preventDefault}>
+            <form action="" className="bg-[#FFFFFF] flex flex-col gap-[40px] p-[40px] rounded-[12px]" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <h1 className={`${H1}`}>Create account</h1>
                     <h3 className={`${P1} text-[#737373]`}>Let’s get you started sharing your links!</h3>
                 </div>
                 <div className="flex flex-col gap-[24px]">
                     <div className="flex flex-col gap-[4px]">
-                        <label htmlFor="email" className={`${P2}`}>Email address</label>
-                        <input type="text" id="email" className={`${inputStyle} bg-[url('/images/icon-email.svg')]`} {...register("emailAddress")} placeholder="e.g. alex@email.com" />
+                        <div className="flex justify-between">
+                            <label htmlFor="email" className={`${P2}`}>Email address</label>
+                            {errors.emailAddress ? <p className={`${P2} text-[#FF3939] relative top-[38px] right-[16px]`}>Can’t be empty</p> : undefined}
+                        </div>
+                        <input type="text" id="email" className={`${inputStyle} bg-[url('/images/icon-email.svg')] ${errors.emailAddress ? "border-[#FF3939]" : undefined}`} {...register("emailAddress")} placeholder="e.g. alex@email.com" />
                     </div>
                     <div className="flex flex-col gap-[4px]">
-                        <label htmlFor="createPassword" className={`${P2}`}>Create password</label>
-                        <input type="text" id="createPassword" className={`${inputStyle} bg-[url('/images/icon-password.svg')]`} {...register("createPassword")} placeholder="At least 8 characters" />
+                        <div className="flex justify-between">
+                            <label htmlFor="createPassword" className={`${P2}`}>Create password</label>
+                            {errors.createPassword ? <p className={`${P2} text-[#FF3939] relative top-[38px] right-[16px]`}>Please check again</p> : undefined}
+                        </div>
+                        <input type="text" id="createPassword" className={`${inputStyle} bg-[url('/images/icon-password.svg')] ${errors.createPassword ? "border-[#FF3939]" : undefined}`} {...register("createPassword")} placeholder="At least 8 characters" />
                     </div>
                     <div className="flex flex-col gap-[4px]">
                         <label htmlFor="confirmPassword" className={`${P2}`}>Confirm password</label>
