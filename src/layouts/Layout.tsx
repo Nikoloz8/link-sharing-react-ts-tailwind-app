f37394dfaf8afd6283af4f14e39d6e7f283a9189
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-15 19:16:55 +0400

    style register form

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="flex items-center p-[50px_0] justify-center min-h-[100vh] bg-[#FAFAFA]">
         <Outlet />
     </div>
   )
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { useForm } from "react-hook-form"
+import tailwind from "../styles/tailwind"
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+
+    const { register, watch } = useForm({
+        defaultValues: {
+            emailAddress: "",
+            createPassword: "",
+            confirmPassword: ""
+        }
+    })
+
+    console.log(watch())
+
+    const { H1, P1, P2, H2, inputStyle } = tailwind()
+
+    return (
+        <div className="flex flex-col gap-[50px] items-center">
+            <img src="/images/logo-devlinks-large.svg" alt="" />
+            <form action="" className="bg-[#FFFFFF] flex flex-col gap-[40px] p-[40px] rounded-[12px]" onSubmit={(e) => e.preventDefault}>
+                <div>
+                    <h1 className={`${H1}`}>Create account</h1>
+                    <h3 className={`${P1} text-[#737373]`}>Let’s get you started sharing your links!</h3>
+                </div>
+                <div className="flex flex-col gap-[24px]">
+                    <div className="flex flex-col gap-[4px]">
+                        <label htmlFor="email" className={`${P2}`}>Email address</label>
+                        <input type="text" id="email" className={`${inputStyle} bg-[url('/images/icon-email.svg')]`} {...register("emailAddress")} placeholder="e.g. alex@email.com" />
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                        <label htmlFor="createPassword" className={`${P2}`}>Create password</label>
+                        <input type="text" id="createPassword" className={`${inputStyle} bg-[url('/images/icon-password.svg')]`} {...register("createPassword")} placeholder="At least 8 characters" />
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                        <label htmlFor="confirmPassword" className={`${P2}`}>Confirm password</label>
+                        <input type="text" id="confirmPassword" className={`${inputStyle} bg-[url('/images/icon-password.svg')]`} {...register("confirmPassword")} placeholder="At least 8 characters" />
+                    </div>
+                    <p className={`${P2} text-[#737373]`}>Password must contains at least 8 characters</p>
+                    <button className={`w-[100%] p-[11px_0] text-center cursor-pointer bg-[#633CFF] rounded-[8px] ${H2} text-[#FFFFFF]`}>
+                        Create new account
+                    </button>
+                    <h4 className={`${P1} text-center text-[#737373]`}>Already have an account? <a className="text-[#633CFF]">Login</a></h4>
+                </div>
+            </form>
+        </div>
+    )
 }
