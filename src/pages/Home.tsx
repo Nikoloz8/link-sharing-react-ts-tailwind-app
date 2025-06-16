3a59036fef5d377ece809995147ab351911fd948
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-17 00:24:23 +0400

    create link add functional in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 import tailwind from "../styles/tailwind"
 
 export default function Home() {
 
-  const { H2, H1, P1 } = tailwind()
+  const { H2, H1, P1, P2, inputStyle } = tailwind()
+
+  const allMenuList = [{ platform: "GitHub", img: "/images/icon-github.svg" }, { platform: "Frontend Mentor", img: "/images/icon-frontend-mentor.svg" }, { platform: "Twitter", img: "/images/icon-twitter.svg" }, { platform: "LinkedIn", img: "/images/icon-linkedin.svg" }, { platform: "YouTube", img: "/images/icon-youtube.svg" }, { platform: "Facebook", img: "/images/icon-facebook.svg" }, { platform: "Twitch", img: "/images/icon-twitch.svg" }, { platform: "Dev.to", img: "/images/icon-devto.svg" }, { platform: "Codewars", img: "/images/icon-codewars.svg" }, { platform: "Codepen", img: "/images/icon-codepen.svg" }, { platform: "freeCodeCamp", img: "/images/icon-freecodecamp.svg" }, { platform: "GitLab", img: "/images/icon-gitlab.svg" }, { platform: "Hashnode", img: "/images/icon-hashnode.svg" }, { platform: "Stack Overflow", img: "/images/icon-stack-overflow.svg" }]
+
+  const [platformLinks, setPlatformLinks] = useState<{ selected: number, link: string, showDropdown: boolean }[]>([])
 
   return (
     <div className="w-[100%] min-h-[100vh] p-[24px] flex flex-col gap-[24px]">
@@ -23,23 +28,82 @@ export default function Home() {
         </button>
       </header>
       <main className="flex gap-[24px] h-[834px]">
-        <section className="w-[560px] min-w-[450px] h-[100%] rounded-[12px] bg-[#FFFFFF] bg-[url('/images/illustration-phone-mockup.svg')] bg-no-repeat bg-center">
+        <section className="w-[560px] min-w-[560px] h-[100%] rounded-[12px] bg-[#FFFFFF] bg-[url('/images/illustration-phone-mockup.svg')] bg-no-repeat bg-center">
         </section>
-        <section className="w-[100%] h-[100%] p-[40px_0_40px_0] flex bg-[#FFFFFF] rounded-[12px] flex-col gap-[40px]">
-          <div className="flex flex-col gap-[8px] p-[0_40px]!">
-            <h1 className={`${H1}`}>Customize your links</h1>
-            <p className={`${P1}`}>Add/edit/remove links below and then share all your profiles with the world!</p>
-          </div>
-          <div className="flex flex-col gap-[24px] p-[0_40px]!">
-            <button className={`w-[100%] p-[11px_0] text-center cursor-pointer ${H2} text-[#633CFF]! border-[1px] border-solid border-[#633CFF]! rounded-[8px]`}>+ Add new link</button>
-            <div className="w-[100%] p-[62.5px_20px] rounded-[12px] bg-[#FAFAFA] flex flex-col items-center gap-[40px]">
-              <img src="/images/illustration-empty.svg" alt="" />
-              <div className="flex flex-col gap-[24px] items-center max-w-[500px]">
-                <h1 className={`${H1}`}>Let’s get you started</h1>
-                <p className={`${P1} text-[#737373] text-center`}>
-                  Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!
-                </p>
-              </div>
+        <section className="w-[100%] h-[100%] pt-[40px] flex bg-[#FFFFFF] rounded-[12px] flex-col justify-between">
+          <div className="flex flex-col gap-[40px]">
+            <div className="flex flex-col gap-[8px] p-[0_40px]!">
+              <h1 className={`${H1}`}>Customize your links</h1>
+              <p className={`${P1}`}>Add/edit/remove links below and then share all your profiles with the world!</p>
+            </div>
+            <div className="flex flex-col gap-[24px] p-[0_40px]!">
+              <button onClick={() => {
+                if (platformLinks.length < 5) {
+                  setPlatformLinks([...platformLinks, {
+                    selected: 0,
+                    link: "",
+                    showDropdown: false
+                  }])
+                }
+              }} className={`w-[100%] p-[11px_0] text-center cursor-pointer ${H2} text-[#633CFF]! border-[1px] border-solid border-[#633CFF]! rounded-[8px]`}>+ Add new link</button>
+              {platformLinks.length !== 0 ? <div className="flex flex-col gap-[24px] overflow-y-scroll h-[510px] pb-[24px]">
+                {platformLinks.map((e, i) => {
+                  return <div key={i} className="bg-[#FAFAFA] rounded-[12px] flex flex-col gap-[12px] p-[20px]">
+                    <div className="w-[100%] flex  justify-between">
+                      <div className="flex gap-[8px] items-center">
+                        <div className="flex flex-col gap-[4px]">
+                          <span className="w-[12px] h-[1px] bg-[#737373]"></span>
+                          <span className="w-[12px] h-[1px] bg-[#737373]"></span>
+                        </div>
+                        <h3 className={`${H2} text-[#737373]`}>Link #{i + 1}</h3>
+                      </div>
+                      <button className={`${P1} text-[#737373] cursor-pointer`} onClick={() => {
+                        setPlatformLinks(platformLinks.filter((_e, index) => i !== index))
+                      }}>Remove</button>
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                      <label className={`${P2}`}>Platform</label>
+                      <div onClick={() => {
+                        setPlatformLinks(prevLinks => prevLinks.map((link, index) => index === i ? { ...link, showDropdown: !link.showDropdown } : link))
+                      }} className={`flex w-[100%] bg-[#FFFFFF] justify-between p-[12px_16px] rounded-[8px] border-[1px] relative border-solid items-center border-[#D9D9D9] ${platformLinks[i].showDropdown ? "border-[#633CFF]! shadow-[0_0_32px_0_rgba(99,60,255,0.25)]" : ""}`}>
+                        <div className={`flex items-center gap-[12px] ${P1}`}>
+                          <img src={allMenuList[e.selected].img} alt="" />
+                          {allMenuList[e.selected].platform}
+                        </div>
+                        <img src="/images/icon-chevron-down.svg" className={`${!platformLinks[i].showDropdown ? "rotate-0" : "rotate-180"} duration-500 transition-all`} alt="" />
+                        <div className={`${!platformLinks[i].showDropdown ? "hidden" : "flex"} absolute w-[100%] p-[0_16px] flex-col bg-[#FFFFFF] rounded-[8px] border-[1px] border-solid border-[#D9D9D9] z-10 top-[64px] right-0`}>
+                          {allMenuList.map((e, ind) => {
+                            return <button onClick={() => {
+                              const newLinks = [...platformLinks]
+                              newLinks[i] = { ...newLinks[i], selected: ind }
+                              setPlatformLinks(newLinks)
+                            }} key={ind} className={`w-[100%] flex gap-[14px] p-[12px_0] border-b-[1px] border-solid border-[#D9D9D9] ${P1} cursor-pointer`}>
+                              <img src={e.img} alt="" />
+                              {e.platform}
+                            </button>
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                      <label htmlFor={`link${i}`} className={`${P2}`}>Link</label>
+                      <input type="text" onChange={(e) => {
+                        const newLinks = [...platformLinks]
+                        newLinks[i] = { ...newLinks[i], link: e.target.value }
+                        setPlatformLinks(newLinks)
+                      }} className={`${inputStyle} w-[100%]! bg-[url('/images/icon-link.svg')]`} id={`link${i}`} placeholder="e.g. https://www.github.com/johnappleseed" />
+                    </div>
+                  </div>
+                })}
+              </div> : <div className="w-[100%] p-[62.5px_20px] rounded-[12px] bg-[#FAFAFA] flex flex-col items-center gap-[40px]">
+                <img src="/images/illustration-empty.svg" alt="" />
+                <div className="flex flex-col gap-[24px] items-center max-w-[500px]">
+                  <h1 className={`${H1}`}>Let’s get you started</h1>
+                  <p className={`${P1} text-[#737373] text-center`}>
+                    Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!
+                  </p>
+                </div>
+              </div>}
             </div>
           </div>
           <div className="p-[24px] flex justify-end items-center border-t-[1px] border-solid border-[#D9D9D9] ">
@@ -49,6 +113,6 @@ export default function Home() {
           </div>
         </section>
       </main>
-    </div>
+    </div >
   )
 }
