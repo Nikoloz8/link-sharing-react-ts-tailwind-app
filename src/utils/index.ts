bab181dc944f743c15c17a3842c8a7dcdd06c1db
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-19 15:10:19 +0400

    delete unnecessary import in index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import type { TFunctions, TUser } from "../types"
 
 export default function index({ watch, reset, platformLinks, image, setImage }: TFunctions) {
